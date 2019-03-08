3ca365acaa21dc75ef992fc7f8c1c6742752217a
Gustavo Luis dos Santos <gustavobhm@bol.com.br>
2019-03-08 09:26:48 -0300

    Aula 2 - Envio de e-mail e JMS

--- casadocodigo/src/main/java/br/com/casadocodigo/loja/service/PagamentoService.java
+++ casadocodigo/src/main/java/br/com/casadocodigo/loja/service/PagamentoService.java
@@ -4,7 +4,11 @@ import java.net.URI;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import javax.annotation.Resource;
 import javax.inject.Inject;
+import javax.jms.Destination;
+import javax.jms.JMSContext;
+import javax.jms.JMSProducer;
 import javax.servlet.ServletContext;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -31,6 +35,12 @@ public class PagamentoService {
 	@Inject
 	private PagamentoGateway pagamentoGateway;
 
+	@Inject
+	private JMSContext jmsContext;
+
+	@Resource(name = "java:/jms/topics/CarrinhoComprasTopico")
+	private Destination destination;
+
 	private static ExecutorService executor = Executors.newFixedThreadPool(50);
 
 	@POST
@@ -39,15 +49,19 @@ public class PagamentoService {
 
 		String contextPath = context.getContextPath();
 
+		JMSProducer producer = jmsContext.createProducer();
+
 		executor.submit(() -> {
 			try {
 				String resposta = pagamentoGateway.pagar(compra.getTotal());
-				System.out.println(resposta);
+
+				producer.send(destination, compra.getUuid());
 
 				URI responseUri = UriBuilder.fromPath("http://localhost:8080" + contextPath + "/index.xhtml")
-						.queryParam("msg", "Compra realizada com sucesso").build();
+						.queryParam("msg", "Compra Realizada com Sucesso").build();
 
 				Response response = Response.seeOther(responseUri).build();
+
 				ar.resume(response);
 			} catch (Exception e) {
 				ar.resume(new WebApplicationException(e));
